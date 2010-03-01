26ad14119b7980f318fb772a780f2b0a89ace36d
Lucas Teixeira <lucastex@gmail.com>
2010-02-28 21:59:37 -0300

    GSolrConfig file

---
+++ grails-app/conf/GSolrConfig.groovy
@@ -0,0 +1,22 @@
+//This file is here for testing purposes
+//It will not be added to the plugin distro
+
+gsolr {
+	solr {
+		products {
+			type = "http"		
+			url = "http://index.hml.br.wcorp.carrefour.com/cf-index/ProductIndex"
+			soTimeout = 1000
+			connectionTimeout = 1000
+			maxTotalConnections = 50
+			followRedirects = false
+			allowCompression = false
+			maxRetries = 1
+		}
+		
+		articles {
+			type = "embeeded"
+			home = "/Volumes/lucas/Java/workspaces/acc/cf/solr-indexes/generator/ArticleIndex/"
+		}
+	}
+}
